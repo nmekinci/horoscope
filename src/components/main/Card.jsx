5424cddb6f2388da6e3b1861f7bd742644eae0d6
Must <emailme>
2023-07-05 10:33:20 +0200

    first commit

---
+++ src/components/main/Card.jsx
@@ -0,0 +1,21 @@
+
+
+const Card = ({title, date,image,desc}) => {
+    // console.log(props);
+    // const {title, date,image,desc} = props;
+    return (
+        <div className="cards">
+            <div className="title">
+                <h1>{title}</h1>
+            </div>
+            <div className="date">
+                <h2>{date}</h2>
+            </div>
+            <img src={image} alt="" />
+            <div className="card-over">
+                <p>{desc}</p>
+            </div>
+        </div>
+    )
+}
+export default Card;
